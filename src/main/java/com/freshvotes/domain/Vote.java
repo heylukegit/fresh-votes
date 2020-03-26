f194dfeb359d505e67ba8fcb9b71f363f9a6a939
luke <mh3994@columbia.edu>
2020-03-26 15:57:06 -0400

    Initial commit

---
+++ src/main/java/com/freshvotes/domain/Vote.java
@@ -0,0 +1,30 @@
+package com.freshvotes.domain;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "vote")
+public class Vote {
+
+    private VoteId pk;
+    private Boolean upvote;
+
+    @EmbeddedId
+    public VoteId getPk() {
+        return pk;
+    }
+
+    public void setPk(VoteId pk) {
+        this.pk = pk;
+    }
+
+    public Boolean getUpvote() {
+        return upvote;
+    }
+
+    public void setUpvote(Boolean upvote) {
+        this.upvote = upvote;
+    }
+}
