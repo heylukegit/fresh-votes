535e39d3267358dfd1cdae52ab5db9186ce519ef
Luke Huang <mh3994@columbia.edu>
2020-05-03 16:28:25 -0400

    FreshVotes 40 - Fixing a Common Bug with my Controller. In the video, Trevor forgot to add Controller Annotation.

--- src/main/java/com/freshvotes/web/FeatureController.java
+++ src/main/java/com/freshvotes/web/FeatureController.java
@@ -1,8 +1,10 @@
 package com.freshvotes.web;
 
+import com.freshvotes.domain.Feature;
 import com.freshvotes.service.FeatureService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,8 +18,13 @@ public class FeatureController {
 
     @PostMapping("/")  // this maps to "/products/{prodcutId}/features"
     public String createFeature(@PathVariable Long productId) {
-        featureService.createFeature(productId);
+        Feature feature = featureService.createFeature(productId);
 
-        return "feature";
+        return "redirect:/products/"+productId+"/features/"+feature.getId();
+    }
+
+    @GetMapping("/{featureId}")
+    public String getFeature(@PathVariable Long productId, @PathVariable Long featureId){
+        return "feature";  // todo add more content
     }
 }
